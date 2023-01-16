ad60d568157c0d7fb0238d2fc1fe1fb81522ede4
Anastasia <a.grebenkina93@gmail.com>
2023-01-16 11:22:49 +0300

    Initial commit

--- src/test/java/QuestionnaireTest.java
+++ src/test/java/QuestionnaireTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Test;
 
 import java.text.SimpleDateFormat;
 import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 
@@ -13,27 +15,26 @@ import static com.codeborne.selenide.Selenide.open;
 
 public class QuestionnaireTest {
 
-    @BeforeEach
-    void setup() {
-        Configuration.holdBrowserOpen = true;
-        Calendar c = new GregorianCalendar();
-        c.add(Calendar.DAY_OF_YEAR, 3);
-        SimpleDateFormat formats = new SimpleDateFormat("dd.MM.yyyy");
-        String str = formats.format(c.getTime());
-        open("http://localhost:9999");
-        $("[data-test-id='date'] input").sendKeys(str);
+    String planningDate = generateDate(3);
+    public String generateDate(int days) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
     }
+
 
     @Test
     void shouldTest(){
         Configuration.holdBrowserOpen = true;
-        open("ttp://localhost:9999/");
+        open("http://localhost:9999/");
         $("[data-test-id='city'] input").setValue("Москва");
+        $("[data-test-id='date'] input").setValue("20.01.2023");
         $("[data-test-id='name'] input").setValue("Анна Иванова-Петрова");
         $("[data-test-id='phone'] input").setValue("+79656667566");
         $("[data-test-id='agreement']").click();
         $(".button").click();
         $("[data-test-id=notification]").shouldBe(Condition.visible, Duration.ofSeconds(11));
+        $(".notification__content")
+                .shouldHave(Condition.text("Встреча успешно забронирована на " + planningDate), Duration.ofSeconds(15))
+                .shouldBe(Condition.visible);
 
     }
 }
